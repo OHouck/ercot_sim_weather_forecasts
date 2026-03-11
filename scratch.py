9befd47b620a07f070007d46f796bba27c63554d
Ozma Houck <ohouck@uchicago.edu>
2026-03-11 16:58:32 -0500

    new analyusis by cluster

--- scratch.py
+++ scratch.py
@@ -1,16 +1,19 @@
-import xarray as xr
 import numpy as np
-path = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ercot_sim_weather_forecasts/raw_data/hrrr_data/wspd/2025/07/hrrr_09z_20250725_f01.nc"
-
-ds = xr.open_dataset(path)
-print(ds)
-
-time = np.unique(ds["time"].values)
-step = np.unique(ds["step"].values)
-valid_time = np.unique(ds["valid_time"].values) 
-
-print("Time:", time)
-print("Step:", step)
-print("Valid Time:", valid_time)
+import pandas as pd
+# path = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ercot_sim_weather_forecasts/raw_data/ercot/np4_160/Settlement_Points_02122026_130727.csv"
+path = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ercot_sim_weather_forecasts/raw_data/ercot/np4_160/Resource_Node_to_Unit_02122026_130727.csv"
 
 
+df = pd.read_csv(path)
+
+
+resource_nodes = df["RESOURCE_NODE"].unique()
+unit_substations = df["UNIT_SUBSTATION"].unique()
+unit_names = df["UNIT_NAME"].unique()
+
+
+
+print(f"Total rows: {len(df)}")
+print(f"Unique RESOURCE_NODE values: {len(resource_nodes)}")
+print(f"Unique UNIT_SUBSTATION values: {len(unit_substations)}")
+print(f"Unique UNIT_NAME values: {len(unit_names)}")
